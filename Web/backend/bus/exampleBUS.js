6b78e6433fec603437a1188482d08d20110a062e
huytienlam <huytienlam@gmail.com>
2025-04-17 12:06:19 +0700

    xoá example

--- Web/backend/bus/exampleBUS.js
+++
@@ -1,9 +0,0 @@
-const ExampleDAO = require("../dao/exampleDAO");
-
-class ExampleBUS {
-  static async layDanhSachKhachHang() {
-    return await ExampleDAO.getDanhSachKhachHang();
-  }
-}
-
-module.exports = ExampleBUS;
